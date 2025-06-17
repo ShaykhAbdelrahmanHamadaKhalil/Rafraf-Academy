ed3c23d4c3a5cd13afc66749624173aef82a561c
Shaykh Abdelrahman Hamada Khalil <abdelrahmanhamada3342@gmail.com>
2025-06-17 17:08:04 +0000

    لا

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,23 @@
-// src/firebase.js
+// File: src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// TODO: Add your Firebase project's configuration here
+// هذا الكود يقرأ المتغيرات بأمان من ملف .env
 const firebaseConfig = {
-  apiKey: "AIzaSyCEru1SwlY458B04uTXDzqU6X3d0ZBbrQQ",
-  authDomain: "rafraf-academy.firebaseapp.com",
-  projectId: "rafraf-academy",
-  storageBucket: "rafraf-academy.firebasestorage.app",
-  messagingSenderId: "19778813268",
-  appId: "1:19778813268:web:0fc3b49d6221f10f611cc1"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-// Initialize Firebase
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
+// Initialize and export Firebase services
+// سنقوم باستدعاء هذه الخدمات من أي مكان في التطبيق
 export const auth = getAuth(app);
+export const db = getFirestore(app);
